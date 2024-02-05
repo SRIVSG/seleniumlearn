20d4a05fef748116975bbf98156d912ec71469a9
sri <sgsrivatsan26@gmail.com>
2024-02-05 13:28:02 +0000

    first add files

---
+++ src/selenium/Firefox.java
@@ -0,0 +1,26 @@
+package selenium;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+public class Firefox {
+
+	public static void main(String[] args) {
+
+float browserversion = 100.2f;
+String browsername= "firfox";
+Boolean isVisible = true;
+int releaseyear = 1998;
+char browserlogo = 'f';
+System.out.println("Version: " + browserversion);
+System.out.println(isVisible);
+System.out.println("Name: " + browsername);
+System.out.println("logo: " + browserlogo);
+System.out.println("year: " + releaseyear);
+
+
+	}
+
+}
